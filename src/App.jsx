1fe9ed68a1bd39a1b3c59410b489869c0b5464d3
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-10-17 11:47:47 -0300

    adicionando assets, global styles e Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+import './global.css'
 
 function App() {
 
   return (
     <div>
+      <Header />
       <Post
         author="Wagner Ferreira"
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora enim porro voluptas libero, sapiente illo magni at sequi sint quisquam! Omnis mollitia in aperiam provident odit assumenda alias illo quaerat?"
