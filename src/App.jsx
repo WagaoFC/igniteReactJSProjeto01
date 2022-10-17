f4e1342e744e5e5602351b983c24988edbaf95e3
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-10-17 10:24:27 -0300

    criando componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Post } from './Post'
 
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Post
+        author="Wagner Ferreira"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora enim porro voluptas libero, sapiente illo magni at sequi sint quisquam! Omnis mollitia in aperiam provident odit assumenda alias illo quaerat?"
+      />
+      <Post
+        author="Wagner Ferreira"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora enim porro voluptas libero, sapiente illo magni at sequi sint quisquam! Omnis mollitia in aperiam provident odit assumenda alias illo quaerat?"
+      />
+    </div>
   )
 }
 
