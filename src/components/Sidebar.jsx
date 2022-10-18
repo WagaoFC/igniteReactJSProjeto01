85fd6f9544a9a770ed45cbc5c5d8701cbeef8c1a
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-10-18 11:05:23 -0300

    criando component avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,9 +11,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar}
-                    src="https://avatars.githubusercontent.com/u/48607604?v=4"
-                />
+                <Avatar src="https://avatars.githubusercontent.com/u/48607604?v=4" />
                 <strong>Wagner Ferreira</strong>
                 <span>Software Developer</span>
             </div>
